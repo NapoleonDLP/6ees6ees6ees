a0f53fa7b2f790a42fc84ab39249146277a9d632
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-04 21:55:57 -0700

    Completes pseudo code and ES6 Grub

--- src/Grub.js
+++ src/Grub.js
@@ -1,2 +1,18 @@
 class Grub {
+  //ES6 uses the constructor method to pass in these properties
+constructor () {
+  //an age property that is set to 0
+  this.age = 0;
+  // a color property that is set to pink
+  this.color = 'pink';
+  // a food property that is set to jelly
+  this.food = 'jelly';
+}
+
+//ES6 sets these properties in a different style
+// an eat method that returns 'Mmmmmmmmm jelly'
+eat () {
+  return `Mmmmmmmmm ${this.food}`
+}
+
 };
