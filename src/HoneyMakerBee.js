dcb2a45d401890bea7ffb5e633a3a9a586d913da
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-04 22:38:10 -0700

    Completes ES6 HoneyMakerBee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,27 +1,31 @@
 // extends Bee
-class HoneyMakerBee {
+class HoneyMakerBee extends Bee {
   // TODO..
   //ES6 uses the constructor and calls super
 // the Bee superclass
-
-// an age property that is set to 10
-
-// a job property that is set to make honey
-
-// a color property inherited from bee that is set to yellow
-
-// a food property that is inherited from grub
-  //no need to adjust if it is inherited
-
+constructor () {
+  super();
+  // an age property that is set to 10
+  this.age = 10;
+  // a job property that is set to make honey
+  this.job = 'make honey';
+  // a color property inherited from bee that is set to yellow
+    //no need to adjust if it is inherited
+  // a food property that is inherited from grub
+    //no need to adjust if it is inherited
+  // a honeyPot property that is set to 0
+  this.honeyPot = 0;
+}
 
 // an eat method that is inherited from grub
   //no need to adjust if it is inherited
 
-
-// a honeyPot property that is set to 0
-
 // a makeHoney method that adds 1 to that honeyBee's honeyPot
-
+makeHoney() {
+  this.honeyPot++;
+}
 // a giveHoney method that subtracts 1 from that honeyBee's honeyPot
-
+giveHoney() {
+  this.honeyPot--;
+}
 };
