3eedbc31a703a8058e66a28412ac24a26dedf2c2
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-04 22:57:21 -0700

    Clean up ghost comments and full ES6 refactor

--- src/Grub.js
+++ src/Grub.js
@@ -1,18 +1,8 @@
 class Grub {
-  //ES6 uses the constructor method to pass in these properties
-constructor () {
-  //an age property that is set to 0
-  this.age = 0;
-  // a color property that is set to pink
-  this.color = 'pink';
-  // a food property that is set to jelly
-  this.food = 'jelly';
-}
-
-//ES6 sets these properties in a different style
-// an eat method that returns 'Mmmmmmmmm jelly'
-eat () {
-  return `Mmmmmmmmm ${this.food}`
-}
-
+  constructor () {
+    this.age = 0;
+    this.color = 'pink';
+    this.food = 'jelly';
+  }
+  eat = () => `Mmmmmmmmm ${this.food}`
 };
