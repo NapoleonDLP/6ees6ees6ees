740227c4e0002aacd0fac3ecf3b8b7584c2b1673
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-04 22:44:11 -0700

    Completed ES6 ForagerBee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,13 +1,24 @@
 //use extends to extend Bee properties
-class ForagerBee {
+class ForagerBee extends Bee {
   // TODO..
 // the Bee superclass
-// an age property that is set to 10
-// a job property that is set to find pollen
-// a color property inherited from bee that is set to yellow
-// a food property that is inherited from grub
-// an eat method that is inherited from grub
-// a canFly property that is set true
-// a treasureChest property that is set to an empty array []
-// a forage method that allows the bee to add a treasure to the treasureChest
+  constructor() {
+    super();
+    // an age property that is set to 10
+    this.age = 10;
+    // a job property that is set to find pollen
+    this.job = 'find pollen';
+    // a color property inherited from bee that is set to yellow
+    // a food property that is inherited from grub
+    // a canFly property that is set true
+    this.canFly = true;
+    // a treasureChest property that is set to an empty array []
+    this.treasureChest = [];
+  }
+  // an eat method that is inherited from grub
+  // a forage method that allows the bee to add a treasure to the treasureChest
+  forage(treasure) {
+    this.treasureChest.push(treasure);
+  }
+
 };
