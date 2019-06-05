0026e105bd6adc1413f46ee8cfb7d8fbc343bc83
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-04 23:00:06 -0700

    Clean up ghost comments and full ES6 refactor

--- src/Bee.js
+++ src/Bee.js
@@ -1,22 +1,8 @@
 class Bee extends Grub {
-// the Grub superclass
-//ES6 style sets the constructor function
-constructor (age, color) {
-  //invokes the super calls the parent constructor
-  //must call super before you can use the keyword this
-  super(age, color);
-  // an age property that is set to 5
-  this.age = 5;
-  // a color property that is set to yellow
-  this.color = 'yellow';
-  // a food property that is inherited from grub
-    //inherited so no need to do anything with this
-
-  // a job property that is set to Keep on growing
-    //when adding new properties they must be set inside the constructor
-  this.job = 'Keep on growing';
-}
-// an eat method that is inherited from grub
-  //inherited no need to do anything
-
+  constructor (age, color) {
+    super(age, color);
+    this.age = 5;
+    this.color = 'yellow';
+    this.job = 'Keep on growing';
+  }
 }
